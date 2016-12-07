4afa9d1260bfcdb5a34710e47dc02764f9991a39
zwubel <zwubel@gmail.com>
2016-12-07 11:31:14 +0100

    Added TCP sending functionality.

--- Unity_Workspace/A2Composer/Assets/readInNetworkData.cs
+++ Unity_Workspace/A2Composer/Assets/readInNetworkData.cs
@@ -6,8 +6,11 @@ public class readInNetworkData : MonoBehaviour {
     Boolean socketReady = false;
     TcpClient mySocket;
     NetworkStream theStream;
-    byte[] buffer;
-    int bufferLength;
+    byte[] readBuffer;
+    byte[] writeBuffer;
+    int readBufferLength;
+    int writeBufferLength;
+    int writeStatus;
     Marker[] markers;
     long frameCounter = 0;
     bool oneMarkerSet = false;
@@ -27,7 +30,9 @@ public class readInNetworkData : MonoBehaviour {
 
     // Initialization
     void Start(){
-        bufferLength = bytesPerMarker * maxMarkerCount + 4; // +4 because ID=-1 marks end of frame
+        readBufferLength = bytesPerMarker * maxMarkerCount + 4; // +4 because ID=-1 marks end of frame
+        writeBufferLength = 4;
+        writeStatus = 0;
         markers = new Marker[maxMarkerCount + 1];
         setupScene = gameObject.GetComponent<setupScene>();
         setupSocket();
@@ -48,37 +53,46 @@ public class readInNetworkData : MonoBehaviour {
     void Update(){
         setupScene.setMarkerArraySet(false);
         oneMarkerSet = false;
-        // Is the socket ready and does it have data waiting?
-        if (socketReady && theStream.DataAvailable){
-            Debug.Log("Socket is ready and stream data is available.");
-            buffer = new byte[bufferLength];
-            int bytesRead = theStream.Read(buffer, 0, bufferLength); // Read socket
-            if (bytesRead == bufferLength) { // Number of bytes read equal to expected number?
-                Debug.Log("bytesRead is equal to bufferLength.");
-                for (int i = 0; i < bufferLength; i += bytesPerMarker){
-                    int curID = System.BitConverter.ToInt32(buffer, i); // ID
-                    if (curID == -1){ // End of frame reached?
-                        Debug.Log("Last masker reached, suspending loop for current frame " + frameCounter + ".");
-                        frameCounter++;
-                        markers[i / bytesPerMarker + 1] = new Marker(-1, 0.0f, 0.0f, 0.0f, 0);
-                        break;
-                    }else if(curID < 0){
-                        Debug.LogError("Marker ID not valid: " + curID);
-                    }else {
-                        float curPosX = System.BitConverter.ToSingle(buffer, i + 4); // X-position
-                        float curPosY = System.BitConverter.ToSingle(buffer, i + 8); // Y-position
-                        float curAngle = System.BitConverter.ToSingle(buffer, i + 12); // Angle
-                        int status = System.BitConverter.ToInt32(buffer, i + 16); // isVisible
-                        markers[i / bytesPerMarker] = new Marker(curID, curPosX, curPosY, curAngle, status); // Add new marker to array
-                        oneMarkerSet = true;
-                        Debug.Log(markers[i / bytesPerMarker].toStr()); // Print debug message containing marker data
+        // Is the socket ready?
+        if (socketReady){
+            writeBuffer = System.BitConverter.GetBytes(writeStatus);
+            theStream.Write(writeBuffer, 0, writeBufferLength);
+
+            // Is the socket ready and does it have data waiting?
+            if (socketReady && theStream.DataAvailable){
+                Debug.Log("Socket is ready and stream data is available.");
+                readBuffer = new byte[readBufferLength];
+                int bytesRead = theStream.Read(readBuffer, 0, readBufferLength); // Read socket
+                if (bytesRead == readBufferLength){ // Number of bytes read equal to expected number?
+                    Debug.Log("bytesRead is equal to bufferLength.");
+                    for (int i = 0; i < readBufferLength; i += bytesPerMarker){
+                        int curID = System.BitConverter.ToInt32(readBuffer, i); // ID
+                        if (curID == -1){ // End of frame reached?
+                            Debug.Log("Last masker reached, suspending loop for current frame " + frameCounter + ".");
+                            frameCounter++;
+                            markers[i / bytesPerMarker + 1] = new Marker(-1, 0.0f, 0.0f, 0.0f, 0);
+                            break;
+                        }
+                        else if (curID < 0){
+                            Debug.LogError("Marker ID not valid: " + curID);
+                        }
+                        else{
+                            float curPosX = System.BitConverter.ToSingle(readBuffer, i + 4); // X-position
+                            float curPosY = System.BitConverter.ToSingle(readBuffer, i + 8); // Y-position
+                            float curAngle = System.BitConverter.ToSingle(readBuffer, i + 12); // Angle
+                            int status = System.BitConverter.ToInt32(readBuffer, i + 16); // isVisible
+                            markers[i / bytesPerMarker] = new Marker(curID, curPosX, curPosY, curAngle, status); // Add new marker to array
+                            oneMarkerSet = true;
+                            Debug.Log(markers[i / bytesPerMarker].toStr()); // Print debug message containing marker data
+                        }
                     }
+                    if (oneMarkerSet)
+                        setupScene.setMarkerArraySet(true);
                 }
-                if(oneMarkerSet)
-                    setupScene.setMarkerArraySet(true);
-            }
-            else{
-                Debug.LogError("Number of bytes read from stream NOT equal to buffer length!");
+                else
+                {
+                    Debug.LogError("Number of bytes read from stream NOT equal to buffer length!");
+                }
             }
         }
     }
